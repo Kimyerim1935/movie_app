e2af08f10c4fd4cec4ceb37e8f6afe686c2afd30
Kim yerim <kimyerim1935@naver.com>
2020-10-02 20:34:19 +0900

    API를 이용한 image, title, summary 삽입

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import axios from 'axios';
+import Movie from './Movie';
+import './App.css';
 
 class App extends React.Component{
 state = {
@@ -11,7 +13,7 @@ getMovies =async () => {
     data : {
       data : { movies } ,
     }
-  } =await axios.get("https://yts-proxy.now.sh/list_movies.json");
+  } =await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
   this.setState({movies , isLoading: false});
 };
 
@@ -19,14 +21,33 @@ componentDidMount(){
 this.getMovies();
 }
 render(){
-  const { isLoading} = this.state;
+  const { isLoading , movies} = this.state;
+return (
+  <section class="container">
+   {isLoading ? (
+     <div class="loader">
+       <span class="loader_text">Loading...</span>
+     </div>
+   )  : (
+     <div class="movies">
+        { movies.map((movie) => (
+                 <Movie 
+                 key={movie.id}
+                 id={movie.id}
+                 year={movie.year}
+                 title={movie.title}
+                 summary={movie.summary}
+                 poster={movie.medium_cover_image}
+               />
+              ))}
+      </div>
+              )} 
 
-return(
-<div>
-{isLoading ? 'Loading...' : 'We are ready'}
-</div>
-)
-}
+    
+  
+    </section>
+    );
+  }
 }
 
 export default App;
