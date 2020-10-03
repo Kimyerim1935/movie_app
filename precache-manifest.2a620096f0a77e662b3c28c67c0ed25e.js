c0ad740c288f127cfab623d6a139a325aaf40b11
Kim yerim <kimyerim1935@naver.com>
2020-10-03 22:07:43 +0900

    Updates

---
+++ precache-manifest.2a620096f0a77e662b3c28c67c0ed25e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcc433838cb4bcb37d66d9da4ae7eb47",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "af0ba30b823c2e514373",
+    "url": "/movie_app/static/css/main.d697e536.chunk.css"
+  },
+  {
+    "revision": "44b7b6c50009b40203cd",
+    "url": "/movie_app/static/js/2.7f999919.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app/static/js/2.7f999919.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af0ba30b823c2e514373",
+    "url": "/movie_app/static/js/main.3219f30f.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
